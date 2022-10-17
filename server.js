4f7e679d2f68376a66449123088779acc27c9645
Khanh Long Nguyen <khanhlong1301@Suijin.local>
2022-10-17 13:24:05 +0700

    Events

--- server.js
+++ server.js
@@ -14,19 +14,64 @@
 // });
 
 //TODO: Express framework
-const express = require('express');
+const express = require("express");
 const app = express();
-const hostname = 'localhost';
+const hostname = "localhost";
 const port = 8017;
 
-const cat = require('./cat');
+const cat = require("./cat");
 
-app.get('/', (req, res) => {
-  let catsArr = ["Maine Coon", "Sphynx", "Toyger", "Balinese", "Burmese", "Russian Blue", "Turkish Van", "Exotic", "Selkirk Rex", "Korat"];
+app.get("/", (req, res) => {
+  let catsArr = [
+    "Maine Coon",
+    "Sphynx",
+    "Toyger",
+    "Balinese",
+    "Burmese",
+    "Russian Blue",
+    "Turkish Van",
+    "Exotic",
+    "Selkirk Rex",
+    "Korat",
+  ];
   let randomCat = cat.getRandomCat(catsArr);
-  res.send(`<h1>Cat: <small>${ randomCat }</small></h1><hr>`);
+
+  //Events
+  const events = require("events");
+  let EventEmitter = new events.EventEmitter();
+  let ringBell = (data) => {
+    console.log("ring ring ring...");
+  };
+  // Lắng nghe sự kiện khi mèo chạy thì gọi tới function ringBell
+  EventEmitter.on("catRun", (data) => ringBell(data));
+
+  // Phát sự kiện con mèo chạy.
+  EventEmitter.emit("catRun", data);
+
+  //Other example
+  const UserModel = require("./UserModel");
+  let User = new UserModel();
+
+  // Vì đã kế thừa events nên class User có thể sử dụng method .on()
+  User.on("saved", (user) => {
+    console.log(`New user saved: ${user.name} - ${user.occupation}`);
+  });
+
+  // Lưu thêm 2 thằng user mới.
+  let newUser = {
+    id: 4,
+    name: "user A",
+    occupation: "Code xịn (─‿‿─)",
+  };
+  User.saveUser(newUser);
+
+  // Lấy ra toàn bộ users
+  let allUser = User.allUser();
+  console.log(allUser);
+
+  res.send(`<h1>Cat: <small>${randomCat}</small></h1><hr>`);
 });
 
 app.listen(port, hostname, () => {
-    console.log(`Hello, I am running at ${ hostname }:${ port }/`);
+  console.log(`Hello, I am running at ${hostname}:${port}/`);
 });
